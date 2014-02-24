df9b01624f61ee881972a210676d2c85e8944421
sldenazis <sldenazis@mailexternos.com.ar>
2014-02-24 18:43:04 -0300

    Cambio validacion por api_key por validacion por ip y dominio, y agrego parametros configurables via config.json.

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var http = require('http');
 var funcs = require('./include/functions/');
-var error = require('./include/errors');
+//var error = require('./include/errors');
 var validate = require('./include/validates');
 var purge = require('./purge.js');
+// Loaf nvpurgator config
+var config = require('./conf/config.json');
 // Load servers data
 var project = require('./conf/project.json');
 
 function serverStart(){
+	//console.log( config.clients_allow );
 
 	http.createServer( function (request, response){
 		if ( validate.method(request.method) ){
-
-			if ( validate.apikey( project, request.headers['x-api-key'] ) ){
+			//if ( validate.apikey( project, request.headers['x-api-key'] ) ){
+			if ( validate.client( funcs.getClient(request), config.clients_allow ) && validate.domain( request.headers.host, project.domain ) ) {
 				funcs.info( 'Host: ' + request.headers.host + ' URL: ' + request.url + ' Method: ' + request.method + '\n' );
 
 				response.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -20,12 +23,21 @@ function serverStart(){
 				purge.file( project, request.url );
 
 			} else {
-				error.print( 403, response );
+				//error.print( 403, response );
+				response.writeHead(403, {
+					'Content-Type': 'text/plain'
+				});
+				response.end('Permission Denied! This incident will be reported.\n');
+				funcs.info('Permission denied! Request client: ' + funcs.getClient(request) + '\n' );
 			}
 		} else {
-			error.print( 501, response );
+			//error.print( 501, response );
+			response.writeHead(501, {
+				'Content-Type': 'text/plain'
+			})
+			response.end('Method not allowed.\n');
 		}
-	}).listen(8000);
+	}).listen( config.port );
 
 	console.log( 'Starting on ' + new Date() + '...\n\nServer running at http://127.0.0.1:8000/\n' );
 }
