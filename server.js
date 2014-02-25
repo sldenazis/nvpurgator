46f869055fe5542aed3861109dfee23c6891f738
sldenazis <sldenazis@gmail.com>
2014-02-25 13:30:46 -0300

    Valido primero ip del cliente y luego metodo

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ var project = require('./conf/project.json');
 
 function serverStart(){
 	http.createServer( function (request, response){
-		/* Valido el metodo - solo deberia aceptar GET - */
-		if ( validate.method(request.method) ){
-			/* Valido la ip del cliente */
-			if ( validate.client( funcs.getClient(request), config.clients_allow ) ) {
+		/* Valido la ip del cliente */
+		if ( validate.client( funcs.getClient(request), config.clients_allow ) ) {
+			/* Valido el metodo - solo deberia aceptar GET - */
+			if ( validate.method(request.method) ){
 				/* Valido el header 'Host:' */
 				if ( validate.domain( request.headers.host, project.domains ) ) {
-					funcs.notice( 'Client: ' + funcs.getClient(request) + ' Host: ' + request.headers.host + ' URL: ' + request.url + ' Method: ' + request.method + '.' );
+					funcs.notice( 'Request: Host: ' + request.headers.host + ', URL: ' + request.url + ', Method: ' + request.method + '. Client: ' + funcs.getClient(request) + '.' );
 
 					response.writeHead(200, { 'Content-Type': 'text/plain' });
 					response.end();
@@ -24,18 +24,16 @@ function serverStart(){
 				} else {
 					response.writeHead(406, { 'Content-Type': 'text/plain' });
 					response.end('Invalid domain \'' + request.headers.host + '\'.\n');
-					funcs.info('Invalid domain \'' + request.headers.host + '\'. Client: ' + funcs.getClient(request) + '\n');
+					funcs.notice('Invalid domain \'' + request.headers.host + '\'. Client: ' + funcs.getClient(request) + '.');
 				}
 			} else {
-				response.writeHead(403, { 'Content-Type': 'text/plain' });
-				response.end('Permission Denied! This incident will be reported.\n');
-				funcs.info('Client not allowed: ' + funcs.getClient(request) + '\n' );
+				response.writeHead(405, { 'Content-Type': 'text/plain' });
+				response.end('Method not allowed.\n');
 			}
 		} else {
-			response.writeHead(405, {
-				'Content-Type': 'text/plain'
-			})
-			response.end('Method not allowed.\n');
+			response.writeHead(403, { 'Content-Type': 'text/plain' });
+			response.end('Permission Denied! This incident will be reported.\n');
+			funcs.notice('Client not allowed: ' + funcs.getClient(request) + '. Request:\tHost: ' + request.headers.host + ', URL: ' + request.url + ', Method: ' + request.method + '.' );
 		}
 	}).listen( config.port );
 
