9828673c7a5958cf3c599904c944425129753381
sldenazis <sldenazis@gmail.com>
2014-02-25 12:41:24 -0300

    Agrego soporte para el nivel de log

--- include/functions/lib/functions.js
+++ include/functions/lib/functions.js
@@ -1,27 +1,35 @@
 function _colorCode( color ){
 	var pcolor = [{
-		"name" : "black"
+		"name" : "black",
+		"id" : 0
 	},{
-		"name" : "red"
+		"name" : "red",
+		"id" : 1
 	},{
-		"name" : "green"
+		"name" : "green",
+		"id" : 2
 	},{
-		"name" : "yellow"
+		"name" : "yellow",
+		"id" : 3
 	},{
-		"name" : "blue"
+		"name" : "blue",
+		"id" : 4
 	},{
-		"name" : "violet"
+		"name" : "violet",
+		"id" : 5
 	},{
-		"name" : "cyan"
+		"name" : "cyan",
+		"id" : 6
 	},{
-		"name" : "white"
+		"name" : "gray",
+		"id" : 7
 	}];
 	
-	var ccode;
+	var ccode = 7;
 	
 	pcolor.forEach(function( c, index ){
 		if ( c.name == color ){
-			ccode = index;
+			ccode = c.id;
 		}
 	})
 
@@ -36,38 +44,64 @@ function _colorLog( color, message ){
 		bold = 1;
 		color = color.substring(1);
 	}
-	var cmessage = '\033[0' + bold + ';3' + _colorCode(color) + 'm' + message + '\033[00;37m';
+	var cmessage = '\033[0' + bold + ';3' + _colorCode(color) + 'm' + message + '\033[0m';
 	return cmessage;
 }
 
 function infoLog(message){
-	var message = '[INFO] ' + message;
-	console.log( _colorLog( 'white', message ) );
+	console.log( _colorLog('green','[INFO] ') + message );
+}
+
+function noticeLog(message){
+	/* noticeLog don't uses colors */
+	console.log(message);
 }
 
 function errorLog(message){
-	var message = '[ERROR] ' + message;
-	console.log( _colorLog( 'nred', message ) );
+	console.log( _colorLog('nred','[ERROR] ') + message );
 }
 
 function warningLog(message){
-	var message = '[WARN] ' + message;
-	console.log( _colorLog( 'red', message ) );
+	console.log( _colorLog('red','[WARN] ') + message );
 }
 
 function debugLog(message){
-	var message = '[DEBUG] ' + message;
-	console.log( _colorLog( 'yellow', message ) );
+	console.log( _colorLog('yellow','[DEBUG] ') + message );
 }
 
-// Devuelvo la ip del cliente
+function noLog(message){
+	return;
+}
+
+/* Devuelvo la ip del cliente */
 function getClientAddress( request ){
 	return (request.headers['x-forwarded-for'] || '').split(',')[0] || request.connection.remoteAddress;
 }
 
+var config = require('../../../conf/config.json');
+var log_level = config.loglevel;
 
-exports.info = infoLog;
 exports.error = errorLog;
+exports.notice = noticeLog;
 exports.warning = warningLog;
+exports.info = infoLog;
 exports.debug = debugLog;
+
+switch(log_level){
+	case 0:
+		exports.warning = noLog;
+		exports.info = noLog;
+		exports.debug = noLog;
+		break;
+	case 1:
+		exports.info = noLog;
+		exports.debug = noLog;
+		break;
+	case 2:
+		exports.debug = noLog;
+		break;
+	default:
+		break;
+}
+
 exports.getClient = getClientAddress;
