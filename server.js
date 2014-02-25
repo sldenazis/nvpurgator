f727d46e8554c4ce75d28fb6dfa30b821be03f92
sldenazis <sldenazis@gmail.com>
2014-02-25 16:38:51 -0300

    Modifico formato del lig

--- server.js
+++ server.js
@@ -9,31 +9,37 @@ var project = require('./conf/project.json');
 
 function serverStart(){
 	http.createServer( function (request, response){
+		var date = funcs.getDate('%Y/%m/%d %H:%M:%S');
+		var client_ip = funcs.getClient(request);
+
 		/* Valido la ip del cliente */
-		if ( validate.client( funcs.getClient(request), config.clients_allow ) ) {
+		if ( validate.client( client_ip, config.clients_allow ) ) {
 			/* Valido el metodo - solo deberia aceptar GET - */
 			if ( validate.method(request.method) ){
 				/* Valido el header 'Host:' */
 				if ( validate.domain( request.headers.host, project.domains ) ) {
-					funcs.notice( 'Request: Host: ' + request.headers.host + ', URL: ' + request.url + ', Method: ' + request.method + '. Client: ' + funcs.getClient(request) + '.' );
-
 					response.writeHead(200, { 'Content-Type': 'text/plain' });
 					response.end();
-
+					funcs.notice('['+ date + '] [Purge "http://' + request.headers.host + request.url + '"] from ' + client_ip + '.');
+					//funcs.logRequest(request,200);
 					purge.file( project, request.url );
 				} else {
 					response.writeHead(406, { 'Content-Type': 'text/plain' });
-					response.end('Invalid domain \'' + request.headers.host + '\'.\n');
-					funcs.notice('Invalid domain \'' + request.headers.host + '\'. Client: ' + funcs.getClient(request) + '.');
+					response.end('Invalid domain "' + request.headers.host + '".\n');
+					funcs.notice('[' + date + '] [Invalid domain "' + request.headers.host + '"] from ' + client_ip + '.');
+					//funcs.logRequest(request,406)
 				}
 			} else {
 				response.writeHead(405, { 'Content-Type': 'text/plain' });
-				response.end('Method not allowed.\n');
+				response.end('Method "' + request.method + '" not allowed.\n');
+				funcs.notice('[' + date + '] [Method "' + request.method + '" not allowed] from ' + client_ip + '.');
+				//funcs.logRequest(request,405);
 			}
 		} else {
 			response.writeHead(403, { 'Content-Type': 'text/plain' });
 			response.end('Permission Denied! This incident will be reported.\n');
-			funcs.notice('Client not allowed: ' + funcs.getClient(request) + '. Request:\tHost: ' + request.headers.host + ', URL: ' + request.url + ', Method: ' + request.method + '.' );
+			funcs.notice('[' + date + '] [Permission denied by config] from ' + client_ip + '. Request: ' + request.method + ' http://' + request.headers.host + request.url);
+			//funcs.logRequest(request,403);
 		}
 	}).listen( config.port );
 
