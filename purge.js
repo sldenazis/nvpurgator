0ed3c89f588bfa68c86975d1c06e4fc55d168166
sldenazis <sldenazis@gmail.com>
2014-02-25 10:22:53 -0300

    Convierto _purge en recursiva para manejar los errores, en principio con una profundidad hardcodeada de 3

--- purge.js
+++ purge.js
@@ -6,7 +6,7 @@ function purgeFileProject( project, path ){
 	if ( project.enabled ){
 		project.servers.forEach(function( server, index ){
 			if ( typeof(server.enabled) === 'undefined' || server.enabled !== false ){
-				// Armo el objeto para la peticion y llamo a _purge
+				/* Armo el objeto para la peticion y llamo a _purge */
 				var port = project.port;
 				if ( typeof(server.port) !== 'undefined' ){
 					port = server.port;
@@ -18,10 +18,8 @@ function purgeFileProject( project, path ){
 					headers: { 'Host' : project.domain },
 					method: 'PURGE'
 				};
-				/* Cantidad de intentos si falla el primer request
-				 * TODO: buscar una forma mejor para manejar los errores
-				 * FIXME: esto no anda */
-				var retry = 0;
+				/* Cantidad de intentos si falla el primer request. TODO: hacerlo configurable */
+				var retry = 2;
 				_purge(request_data, retry);
 			} else {
 				funcs.info( 'The requested server \'' + server.ip + '\' has been disable by config.' );
@@ -41,21 +39,18 @@ function _purge(request_data, retry){
 		response.resume();
 	});
 
-	var recall = false;
-	recall = request.on('error', function(e) {
-		funcs.error( 'Problem with request in host ' + request_data.hostname  + ': ' + e.message);
-		return true;
-	});
-
 	request.end();
 
-	if ( recall ){
-		if( retry > 1 ){
+	request.on('error', function(e) {
+		funcs.error( '[' + retry + '] Problem with request in host ' + request_data.hostname  + ': ' + e.message);
+		if( retry > 0 ){
+			/* Espero un segundo para volver a realizar el request.
+			 * TODO: hacerlo configurable */
 			setTimeout(function(){
 				_purge(request_data, retry - 1);
 			}, 1000);
 		}
-	}
+	});
 }
 
 exports.file = purgeFileProject;
