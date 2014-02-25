c46163471613e7031d92e1089dd4e1817620ee56
sldenazis <sldenazis@gmail.com>
2014-02-25 11:29:38 -0300

    Quito validacion por api-key y valido por dominio e ip del cliente. Modifico la llamada a validate.domain() para pasarle un array de dominios.

--- server.js
+++ server.js
@@ -2,31 +2,34 @@ var http = require('http');
 var funcs = require('./include/functions/');
 var validate = require('./include/validates');
 var purge = require('./purge.js');
-// Loaf nvpurgator config
+/* Load nvpurgator config */
 var config = require('./conf/config.json');
-// Load servers data
+/* Load servers data */
 var project = require('./conf/project.json');
 
 function serverStart(){
-	//console.log( config.clients_allow );
-
 	http.createServer( function (request, response){
+		/* Valido el metodo - solo deberia aceptar GET - */
 		if ( validate.method(request.method) ){
-			//if ( validate.apikey( project, request.headers['x-api-key'] ) ){
-			if ( validate.client( funcs.getClient(request), config.clients_allow ) && validate.domain( request.headers.host, project.domain ) ) {
-				funcs.info( 'Host: ' + request.headers.host + ' URL: ' + request.url + ' Method: ' + request.method + '\n' );
+			/* Valido la ip del cliente */
+			if ( validate.client( funcs.getClient(request), config.clients_allow ) ) {
+				/* Valido el header 'Host:' */
+				if ( validate.domain( request.headers.host, project.domains ) ) {
+					funcs.info( 'Host: ' + request.headers.host + ' URL: ' + request.url + ' Method: ' + request.method + '\n' );
 
-				response.writeHead(200, { 'Content-Type': 'text/plain' });
-				response.end();
+					response.writeHead(200, { 'Content-Type': 'text/plain' });
+					response.end();
 
-				purge.file( project, request.url );
-
+					purge.file( project, request.url );
+				} else {
+					response.writeHead(406, { 'Content-Type': 'text/plain' });
+					response.end('Invalid domain \'' + request.headers.host + '\'.\n');
+					funcs.info('Invalid domain \'' + request.headers.host + '\'. Client: ' + funcs.getClient(request) + '\n');
+				}
 			} else {
-				response.writeHead(403, {
-					'Content-Type': 'text/plain'
-				});
+				response.writeHead(403, { 'Content-Type': 'text/plain' });
 				response.end('Permission Denied! This incident will be reported.\n');
-				funcs.info('Permission denied! Request client: ' + funcs.getClient(request) + '\n' );
+				funcs.info('Client not allowed: ' + funcs.getClient(request) + '\n' );
 			}
 		} else {
 			response.writeHead(501, {
