91eec0890f65ad18a93bd42a76b7c2b166bc00d5
sldenazis <sldenazis@mailexternos.com.ar>
2014-02-26 14:37:28 -0300

    Testing con pidfile

--- nvpurgator.js
+++ nvpurgator.js
@@ -4,6 +4,15 @@ var validate = require('./include/validates');
 var config = require('./conf/config.json');
 var server = require('./server.js');
 
-if( validate.config(config) ){
-	server.start();
-}
+/* Write pid or die. TODO: Remove this from here. */
+var file = require('fs');
+
+file.writeFile(config.pidfile, process.pid, function(err) {
+    if(err) {
+        console.log('Unable to write pidfile "' + config.pidfile + '"\nError: ' + err);
+    } else {
+		if( validate.config(config) ){
+			server.start();
+		}
+    }
+});
